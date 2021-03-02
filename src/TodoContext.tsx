2e593baa62e93e71c72ebabd8df12ea0c7e0df9c
igorashs <igorashsirbu@gmail.com>
2021-03-02 15:01:59 +0200

    refactor TodoStore

--- src/TodoContext.tsx
+++ src/TodoContext.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext } from 'react';
-import { makeAutoObservable } from 'mobx';
 import TodoStore from './TodoStore';
 
 const TodoContext = createContext<TodoStore>({} as TodoStore);
@@ -9,10 +8,10 @@ type Props = {
 };
 
 const TodoProvider = ({ children }: Props) => {
-  const todoStore = makeAutoObservable(new TodoStore());
-
   return (
-    <TodoContext.Provider value={todoStore}>{children}</TodoContext.Provider>
+    <TodoContext.Provider value={new TodoStore()}>
+      {children}
+    </TodoContext.Provider>
   );
 };
 
--- src/TodoStore.ts
+++ src/TodoStore.ts
@@ -1,18 +1,30 @@
 import { nanoid } from 'nanoid';
+import { makeAutoObservable } from 'mobx';
 
 interface Todo {
   text: string;
   id: string;
+  isCompleted: boolean;
 }
 
 export default class TodoStore {
   todos: Array<Todo> = [];
 
-  addTodo(text: string) {
-    this.todos.push({ text, id: nanoid() });
+  constructor() {
+    makeAutoObservable(this);
   }
 
-  removeTodo(id: string) {
+  addTodo = (text: string) => {
+    this.todos.push({ text, id: nanoid(), isCompleted: false });
+  };
+
+  removeTodo = (id: string) => {
     this.todos = this.todos.filter((t) => t.id !== id);
-  }
+  };
+
+  toggleCompleted = (id: string) => {
+    this.todos = this.todos.map((t) =>
+      t.id === id ? { ...t, isCompleted: !t.isCompleted } : t,
+    );
+  };
 }
