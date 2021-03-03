6c91cd92d488f6e68521ab1a2263f1fc8afe1806
igorashs <igorashsirbu@gmail.com>
2021-03-03 10:59:43 +0200

    add exports

--- src/TodoContext.tsx
+++ src/TodoContext.tsx
@@ -1,13 +1,13 @@
 import React, { createContext, useContext } from 'react';
 import TodoStore from './TodoStore';
 
-const TodoContext = createContext<TodoStore>({} as TodoStore);
+export const TodoContext = createContext<TodoStore>({} as TodoStore);
 
 type Props = {
   children: React.ReactNode;
 };
 
-const TodoProvider = ({ children }: Props) => {
+export const TodoProvider = ({ children }: Props) => {
   return (
     <TodoContext.Provider value={new TodoStore()}>
       {children}
@@ -16,5 +16,3 @@ const TodoProvider = ({ children }: Props) => {
 };
 
 export const useTodoStore = () => useContext(TodoContext);
-
-export default TodoProvider;
