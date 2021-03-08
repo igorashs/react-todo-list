b831c9cdc30af5ffc81462449b989d37c63806a6
igorashs <igorashsirbu@gmail.com>
2021-03-08 12:47:35 +0200

    export TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import {
-  Button,
-  List,
-  ListItem,
-  Typography,
-  Grid,
-  Box,
-  Paper,
-  Container,
-} from '@material-ui/core';
-import { observer } from 'mobx-react-lite';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { useTodoStore } from './TodoContext';
+import { Grid, Container } from '@material-ui/core';
+
 import Header from './Header';
+import TodoList from './TodoList';
 import NewTodoForm from './NewTodoForm';
 
-const App = observer(() => {
-  const { todos, toggleCompleted, removeTodo } = useTodoStore();
-
+const App = () => {
   return (
     <>
       <Header />
@@ -24,49 +12,13 @@ const App = observer(() => {
       <Container maxWidth="sm">
         <Grid container direction="column">
           <Grid item>
-            <List>
-              {!todos.length ? (
-                <Typography>Todo List is empty</Typography>
-              ) : (
-                todos.map((todo) => (
-                  <ListItem
-                    key={todo.id}
-                    button
-                    onClick={() => toggleCompleted(todo.id)}
-                  >
-                    <Container disableGutters>
-                      <Paper>
-                        <Box p={2}>
-                          <Typography
-                            style={{
-                              textDecoration: todo.isCompleted
-                                ? 'line-through'
-                                : 'none',
-                              wordBreak: 'break-all',
-                            }}
-                          >
-                            {todo.text}
-                          </Typography>
-                          <Button
-                            variant="contained"
-                            color="secondary"
-                            onClick={() => removeTodo(todo.id)}
-                          >
-                            <DeleteIcon />
-                          </Button>
-                        </Box>
-                      </Paper>
-                    </Container>
-                  </ListItem>
-                ))
-              )}
-            </List>
+            <TodoList />
             <NewTodoForm />
           </Grid>
         </Grid>
       </Container>
     </>
   );
-});
+};
 
 export default App;
---
+++ src/TodoList.tsx
@@ -0,0 +1,58 @@
+import { observer } from 'mobx-react-lite';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  Box,
+  Button,
+  Container,
+  List,
+  ListItem,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+import { useTodoStore } from './TodoContext';
+
+const TodoList = observer(() => {
+  const { todos, removeTodo, toggleCompleted } = useTodoStore();
+
+  return (
+    <List>
+      {!todos.length ? (
+        <Typography>Todo List is empty</Typography>
+      ) : (
+        todos.map((todo) => (
+          <ListItem
+            key={todo.id}
+            button
+            onClick={() => toggleCompleted(todo.id)}
+          >
+            <Container disableGutters>
+              <Paper>
+                <Box p={2}>
+                  <Typography
+                    style={{
+                      textDecoration: todo.isCompleted
+                        ? 'line-through'
+                        : 'none',
+                      wordBreak: 'break-all',
+                    }}
+                  >
+                    {todo.text}
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => removeTodo(todo.id)}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                </Box>
+              </Paper>
+            </Container>
+          </ListItem>
+        ))
+      )}
+    </List>
+  );
+});
+
+export default TodoList;
